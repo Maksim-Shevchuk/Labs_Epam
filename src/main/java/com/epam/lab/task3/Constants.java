b00f1811151f5fc5042fd8c12899180fe2e3e39a
Maksim Shevchuk <maskim27012003@gmail.com>
2022-04-16 13:59:15 +0300

    updated task3

--- src/main/java/com/epam/lab/task3/Constants.java
+++ src/main/java/com/epam/lab/task3/Constants.java
@@ -3,8 +3,8 @@ package com.epam.lab.task3;
 import java.nio.file.Paths;
 
 public interface Constants {
-    String SCHEME_PATH = Paths.get("src", "main", "java", "com", "epam", "lab", "task3", "webPages.xsd").toAbsolutePath().toString();
-    String XML_PATH = Paths.get("src","main","java", "com", "epam", "lab","task3", "webPages.xml").toAbsolutePath().toString();
-    String NEW_XML_PATH = Paths.get("src","main","java", "com", "epam", "lab","task3", "webPagesNew.xml").toAbsolutePath().toString();
+    String SCHEME_PATH = Paths.get("src", "main", "java", "com", "epam", "lab", "task3","resources", "webPages.xsd").toAbsolutePath().toString();
+    String XML_PATH = Paths.get("src","main","java", "com", "epam", "lab","task3", "resources", "webPages.xml").toAbsolutePath().toString();
+    String NEW_XML_PATH = Paths.get("src","main","java", "com", "epam", "lab","task3", "resources", "webPagesNew.xml").toAbsolutePath().toString();
 
 }
